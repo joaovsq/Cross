74f33def86180cdc7aaae477b10ec5eaf6d774b2
Joao V <29417278+joaovsq@users.noreply.github.com>
2020-11-20 05:30:10 -0400

    Editor fileBrowser moved to local scope

--- Source/Editor/EditorApplication.cpp
+++ Source/Editor/EditorApplication.cpp
@@ -12,7 +12,6 @@ namespace CrossEditor {
 
 EditorApplication::EditorApplication(Context* context)
 	:Application(context) {
-
 	editorTitle_ = "Cross Application";
 	editor_ = new Editor(context);
 }
@@ -23,6 +22,8 @@ void EditorApplication::Setup()
 	engineParameters_[EP_WINDOW_TITLE] = GetTypeName();
 	engineParameters_[EP_WINDOW_WIDTH] = 1280;
 	engineParameters_[EP_WINDOW_HEIGHT] = 720;
+	engineParameters_[EP_WINDOW_RESIZABLE] = true;
+	engineParameters_[EP_VSYNC] = true;
 	engineParameters_[EP_LOG_NAME] = GetSubsystem<FileSystem>()->GetAppPreferencesDir("cross", "logs") + GetTypeName() + ".log";
 	engineParameters_[EP_FULL_SCREEN] = false;
 	engineParameters_[EP_HEADLESS] = false;
